b79a4a62af83c3bf58c5dcf7c994cbd9b5769d86
dimerbwimba <dimerbwimba191996@gmail.com>
2024-02-17 19:03:19 +0100

    update the login and register

--- app/(admin)/error/page.tsx
+++ app/(admin)/error/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { useRouter, useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 
 const Error = () => {
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const errMsg = searchParams.get("error")
+  // const searchParams = useSearchParams()
+  // const errMsg = searchParams.get("error")
 
   return (
     <section className=" h-screen justify-center items-center flex">
       <div>
-        <h1>Errors: {errMsg}</h1>
+        {/* <h1>Errors: {errMsg}</h1> */}
         <Button
           onClick={() => router.back()}
           className="w-full mt-2"
