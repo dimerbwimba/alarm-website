bf0ff22f7b53bb1bd1703539be7abd88fde41733
dimerbwimba <dimerbwimba191996@gmail.com>
2024-02-22 15:12:20 +0100

    Admin Update Mysite | hero section

--- lib/compressor.ts
+++ lib/compressor.ts
@@ -1,13 +1,12 @@
-// utils/compressImage.ts
 import imageCompression from 'browser-image-compression';
 
-const compressImage = async (file: any, options:any): Promise<File | String> => {
+const compressImage = async (file: any, options: any): Promise<File> => {
   try {
     const compressedFile = await imageCompression(file, options);
-    return compressedFile;
+    return compressedFile as File;
   } catch (error) {
     console.error('Image compression error:', error);
-    return "null";
+    throw error; // Rethrow the error to handle it in the caller
   }
 };
 
