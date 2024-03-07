b748683478fb1f9f27c32b509d5ae5d414b898bc
dimerbwimba <dimerbwimba191996@gmail.com>
2024-03-07 10:40:46 +0100

    update errors

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -1,56 +1,29 @@
-"use client"
 
 import HeroWithBG from "@/app/_components/hero_bg";
-import BreadCrumb from "@/components/breadcrumb";
-import BreadCrumbFront from "@/components/breadcrumb-front";
-import axios from "axios";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { fetcher } from "@/lib/utils";
+import { cache } from "react";
+import { notFound } from "next/navigation";
 
-const Blog = () => {
-    const [loading, setLoading] = useState(true)
-    const [blog, setBlog] = useState<any>({})
-    const params = useParams()
-    const getBlog = async () => {
-        await axios.get(`/api/public/blogs/blog?slug=${params.slug}`).then(({data})=>{
-            if (!data.error) {
-                
-                setBlog(data.blog)
-                setLoading(false)
-            }
-        })
-    }
-    
-    useEffect(()=>{
-        getBlog()
-    },[])
 
-    if (loading) {
-        return (<div aria-label="Loading..." role="status" className="flex justify-center items-center h-screen space-x-2">
-            <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
-                <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-                <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-                <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-            </svg>
-            <span className="text-4xl font-medium text-gray-500">Loading...</span>
-        </div>)
+interface SingleBlogPageProps {
+    params: { slug: string, name:string }
+}
+
+
+const getBlog = cache(async(slug:string) => {
+    const response = await fetcher(`/api/public/blogs/blog?slug=${slug}`)
+    const { blog } = await response
+    if (!blog) {
+        notFound()
     }
+    return blog
+})
+
+export default async function SingleBlogPage({ params }: SingleBlogPageProps) {
     
-    const breadcrumbItems = [
-        { title: blog.title, link: `/blog/${blog?._id}` }
-    ];
-    
+    const blog = await getBlog(params.slug)
+
+    // const breadcrumbItems = [{}]
     return ( 
         <div>
             <div className="w-full ">
@@ -58,7 +31,7 @@ const Blog = () => {
             </div>
             <div className=" flex justify-center">
                 <div className="lg:w-1/2 text-lg ">
-                <BreadCrumbFront items={breadcrumbItems}/>
+                {/* <BreadCrumbFront items={breadcrumbItems}/> */}
                     <div className=" mx-10">
                         <p className=" py-10"> {blog?.description}</p>
                     <img src={blog.image} className=" border p-2 h-96 rounded object-cover my-5 " />
@@ -73,4 +46,3 @@ const Blog = () => {
      );
 }
  
-export default Blog;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
+import axios from "axios";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const baseURL = "http://localhost:3000"; // Set your API base URL in your .env.local file
+
+const axiosFetcher = axios.create({
+  baseURL,
+});
+
+export const fetcher = async (url:string) => {
+  try {
+    const response = await axiosFetcher.get(url);
+    return response.data;
+  } catch (error:any) {
+    console.error('Error fetching data:', error.message);
+     throw error;
+  }
+};
