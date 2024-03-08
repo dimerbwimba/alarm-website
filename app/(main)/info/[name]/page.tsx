972af8fddc9e37df830de9b889b3cd8b61b9e9a2
dimerbwimba <dimerbwimba191996@gmail.com>
2024-03-08 12:04:38 +0100

    update changes

--- app/(main)/info/[name]/page.tsx
+++ app/(main)/info/[name]/page.tsx
@@ -68,49 +68,12 @@ const AgentInfo: FC = () => {
   return (
     <div className="">
       <div className="w-full ">
-        <HeroWithBG image_url="https://i.ibb.co/y4TR8Fc/DSC-0834-compressed.jpg" span_title="Alarm RDC" title={`CV | ${cvData?.agent?.name} `} />
+        <HeroWithBG image_url="https://i.ibb.co/y4TR8Fc/DSC-0834-compressed.jpg" span_title="Alarm RDC" title={`CV`} />
       </div>
       <div className=" flex justify-center">
-        <div className=" md:w-5/6 px-2">
-          <div className=" grid md:grid-cols-12">
-            <div className=" col-span-5 flex bg-white rounded-lg p-5 space-x-4 h-48">
-              <img src={cvData?.agent?.image} className="shadow-lg rounded-lg" alt="" />
-              <div className="">
-                <div className="flex  mb-2 flex-col gap-3">
-                    <ul className="space-y-3">
-                        <li className="flex items-center gap-5 group">
-                            <a href="" className="h-10 w-10 inline-flex items-center justify-center border border-default-300 text-default-800 rounded-lg transition-all duration-700 group-hover:bg-primary group-hover:text-white group-hover:border-primary">
-                              <PhoneCall/>
-                            </a>
-                            <h5 className="text-base font-medium text-default-800">Appelle Direct</h5>
-                        </li>
-                        <li className="flex items-center gap-5 group">
-                            <a href="" className="h-10 w-10 inline-flex items-center justify-center border border-default-300 text-default-800 rounded-lg transition-all duration-700 group-hover:bg-primary group-hover:text-white group-hover:border-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lucide="facebook" className="lucide lucide-facebook h-5 w-5"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg></a>
-                            <h5 className="text-base font-medium text-default-800">Ecrie moi sur Facebook</h5>
-                        </li>
-                        <li className="flex items-center gap-5 group">
-                            <a href="" className="h-10 w-10 inline-flex items-center justify-center border border-default-300 text-default-800 rounded-lg transition-all duration-700 group-hover:bg-primary group-hover:text-white group-hover:border-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-lucide="linkedin" className="lucide lucide-linkedin h-5 w-5"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
-                            <h5 className="text-base font-medium text-default-800">Rejoint moi sure Linkedin</h5>
-                        </li>
-                        <Separator/>
-                        <li className=" gap-5 group flex space-y-2 mt-2">
-                            <a href="" className="h-10 w-12 inline-flex items-center justify-center border border-default-300 text-default-800 rounded-lg transition-all duration-700 group-hover:bg-primary group-hover:text-white group-hover:border-primary">
-                              <ScanFace/>
-                            </a>
-                          <h5 className="text-black">{cvData?.agent?.name}</h5>
-
-                        </li>
-                        <li className="gap-5 group flex space-y-2 mt-2">
-                          <a href="" className="h-10 w-14 inline-flex items-center justify-center border border-default-300 text-default-800 rounded-lg transition-all duration-700 group-hover:bg-primary group-hover:text-white group-hover:border-primary">
-                              <LucideWorkflow/>
-                            </a>
-                          <h5 className="text-black">{cvData?.agent?.fonction}</h5>
-                        </li>
-                    </ul>
-                </div>
-              </div>
-            </div>
-            <div className=" bg-gray-50 lg:pl-5 border-l-4 border-yellow-700 py-2 md:col-span-7 ">
+        <div className=" md:w-1/2 px-2">
+          <div className="">
+            <div className=" bg-gray-50 lg:pl-5 border-l-4 border-yellow-700 py-2 ">
               <div className="prose  prose-a:text-blue-600 prose-md prose-td:border prose-td:px-2 prose-td:py-1 prose-td:text-sm prose-th:bg-blue-400 md:p-5 p-2" dangerouslySetInnerHTML={{ __html: `${cvData?.html_body}` }}></div>
             </div>
           </div>
--- app/api/public/single-agent/route.ts
+++ app/api/public/single-agent/route.ts
@@ -8,10 +8,10 @@ connectToMongoDB()
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
   const id = searchParams.get('id') as string
-    const objId = new mongoose.Types.ObjectId(id);
-
-    try {
-        const cv = await Cv.findOne({agent:objId}).populate('agent');
+  
+  try {
+      const objId = new mongoose.Types.ObjectId(id);
+        const cv = await Cv.findOne({agent:objId});
         if (cv) {
           return NextResponse.json({ cv, message: "Tu viens de recevoir le CV d'un agent", error: false });
         } else {
